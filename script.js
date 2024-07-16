d2dddaffdc26630fdeb403f4209d6d7e0bd55647
Jwery <weryjoahchim@gmail.com>
2024-07-16 16:48:17 +0200

    mobile version updated

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-// no js here
+document.addEventListener('DOMContentLoaded', function() {
+    const menuIcon = document.querySelector('.menu-icon');
+    const bot = document.querySelector('.bot');
+
+    menuIcon.addEventListener('click', function() {
+        bot.classList.toggle('active');
+        menuIcon.classList.toggle('change');
+    });
+});
